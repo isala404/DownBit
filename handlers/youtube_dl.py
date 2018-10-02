cdafac8611f99ad7f3b9fd0f5bbdf3fe5bb095d8
Isala Piyarisi <isalap99@gmail.com>
2018-10-02 14:45:20 +0530

    Completed Youtube DL Handler

--- handlers/youtube_dl.py
+++ handlers/youtube_dl.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import sys
 import eyed3
 from Youtube_DL import youtube_dl
+import os
 if sys.version_info >= (2, 7):
     pass
 else:
@@ -35,17 +36,18 @@ def youtube_download_mp3(track_name, artist_name, album_name, cover_image, path)
         ydl.download(['ytsearch:{} {} Audio'.format(artist_name, track_name)])
 
     song_path = \
-        [i for i in os.listdir(path) if '{} - {}'.format(ArtistName, TrackName) in i and i.endswith('.mp3')][0]
+        [i for i in os.listdir(path) if '{} - {}'.format(album_name, track_name) in i and i.endswith('.mp3')][0]
     saved_name = os.path.join(path, song_path)
-    audiofile = eyed3.load(saved_name)
-    audiofile.tag.artist = u"{}".format(name.split(';;;')[1])
-    audiofile.tag.album = u"{}".format(album)
-    audiofile.tag.album_artist = u"{}".format(ArtistName)
-    dl_dir = "/tmp/{}-{}.jpg".format(TrackName, ArtistName)
-    urllib.request.urlretrieve(img_url, dl_dir)
-    audiofile.tag.title = u"{}".format(TrackName)
-    audiofile.tag.images.set(3, open(dl_dir, "rb").read(), "image/jpeg", u"")
-    audiofile.tag.save()
+
+    audio_file = eyed3.load(saved_name)
+    audio_file.tag.artist = u"{}".format(artist_name)
+    audio_file.tag.album = u"{}".format(album_name)
+    audio_file.tag.album_artist = u"{}".format(artist_name)
+    dl_dir = "/tmp/{}-{}.jpg".format(artist_name, track_name)
+    urllib.request.urlretrieve(cover_image, dl_dir)
+    audio_file.tag.title = u"{}".format(track_name)
+    audio_file.tag.images.set(3, open(dl_dir, "rb").read(), "image/jpeg", u"")
+    audio_file.tag.save()
 
 
 def get_quality(s):
