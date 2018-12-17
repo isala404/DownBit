d3411f0654d7a7c01861304c4d0bb7421dc92cbc
Isala Piyarisi <isalap99@gmail.com>
2018-12-17 12:27:28 +0530

    Added Settings Files

---
+++ settings.py
@@ -0,0 +1,6 @@
+song_download_path = '/mnt/Music/'
+spotify_token = ''
+youtube_download_path = '/mnt/Youtube/'
+crawler_time_out = 300
+downloader_time_out = 300
+download_hours = [0, 1, 2, 3, 4, 5, 6, 7]
