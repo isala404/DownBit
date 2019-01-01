19c6627d60d0214e4de921d35c056482d4fde310
Isala Piyarisi <isalap99@gmail.com>
2019-01-01 19:10:25 +0530

    Added Plugin to Download SP Playlists

--- ext_plugins/spotify_playlist.py
+++ ext_plugins/spotify_playlist.py
@@ -4,8 +4,7 @@ import eyed3
 from urllib.request import urlretrieve as download
 from DownBit import *
 
-sp = spotipy.Spotify(
-    auth='BQDfnPtqlmpUjr79xm8W6kfVR_GlSo0Lt6mbol3L9ZGHUMPb5ngkE-m-9vm3atsDvJr3omXL8fLWUCwaLw0GgxBt09yeuc_X9SX3NfGIpWk_K89P20kjKWfZmmAall0X99y2-QWoomM9a-2txCtLL3Bk77PeH-vdODPxh0QtKfVuVA')
+sp = spotipy.Spotify(auth='')
 
 results = sp.user_playlist_tracks('4G9wQldgTi2uWuI2i2yaWQ', '37i9dQZF1DXdxcBWuJkbcy')
 tracks = results['items']
@@ -15,13 +14,14 @@ while results['next']:
 
 for item in tracks:
     track = item['track']
-    vid = track['id'],
-    track_name = track['name'],
-    artist_name = track['artists'][0]['name'],
-    album_name = track['album']['name'],
-    album_artist = track['album']['artists'][0]['name'],
-    image = track['album']['images'][0]['url'],
+    vid = track['id']
+    track_name = track['name']
+    artist_name = track['artists'][0]['name']
+    album_name = track['album']['name']
+    album_artist = track['album']['artists'][0]['name']
+    image = track['album']['images'][0]['url']
     release_date = track['album']['release_date']
+    print("{} - {}".format(artist_name, track_name))
 
     ydl_opts = {
         'format': 'bestaudio/best',
@@ -32,15 +32,14 @@ for item in tracks:
         'outtmpl': '/root/Music/{} - {} [%(id)s].%(ext)s'.format(safe_filename(artist_name),
                                                                  safe_filename(track_name)),
         'continuedl': True,
-        'logger': logger,
         'default_search': 'auto',
         'noplaylist': True,
         'source_address': '0.0.0.0',
         'noprogress': True
     }
-    logger.info("[Spotify] Downloading {} by {}".format(track_name, artist_name))
+    logger.info("[Spotify Playlist] Downloading {} by {}".format(track_name, artist_name))
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        data = ydl.extract_info("ytsearch:{} {} audio".format(track['artists'][0]['name'], track['name']),
+        data = ydl.extract_info("ytsearch:{} {} audio".format(artist_name, track_name),
                                 download=False)
         path = ydl.prepare_filename(data)
 
@@ -52,11 +51,10 @@ for item in tracks:
         path = '.'.join(path.split('.')[:-1]) + '.wav'
 
     if not os.path.exists(path):
-        logger.error('Audio File was not found')
-        logger.error(
-            '{}, {}, {}, {}, {}, {}, {}, {}'.format(vid, track_name, artist_name, album_name,
-                                                    album_artist, image, album_artist,
-                                                    release_date))
+        print('Audio File was not found')
+        print('{}, {}, {}, {}, {}, {}, {}, {}'.format(vid, track_name, artist_name, album_name,
+                                                      album_artist, image, album_artist,
+                                                      release_date))
         continue
 
     dl_dir = "/tmp/{}-{}.jpg".format(safe_filename(track_name), safe_filename(artist_name))
